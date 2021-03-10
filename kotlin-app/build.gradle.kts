116c17cc57509c6e682fe656690164aa6aa98bed
Igor Akkerman <commit@dynamite.software>
2021-03-11 00:30:13 +0100

    Gradle and dependencies upgraded

--- kotlin-app/build.gradle.kts
+++ kotlin-app/build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.30"
-    kotlin("kapt") version "1.4.30"
-    kotlin("plugin.spring") version "1.4.21"
+    kotlin("jvm") version "1.4.31"
+    kotlin("kapt") version "1.4.31"
+    kotlin("plugin.spring") version "1.4.31"
     id("org.springframework.boot") version "2.4.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
@@ -22,7 +22,7 @@ dependencies {
     implementation("org.springframework.kafka:spring-kafka")
     testImplementation("org.springframework.kafka:spring-kafka-test")
 
-    implementation("io.github.microutils:kotlin-logging-jvm:2.0.4")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.4")
 
@@ -33,7 +33,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 
-    testImplementation("io.kotest:kotest-assertions-core-jvm:4.4.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.4.3")
     testImplementation("com.ninja-squad:springmockk:3.0.1")
 
     testImplementation("org.testcontainers:junit-jupiter:1.15.2")
