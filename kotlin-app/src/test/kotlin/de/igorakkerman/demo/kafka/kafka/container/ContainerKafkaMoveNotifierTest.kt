2bedabda1e30abc3e61984a575fb61b7005301b2
Igor Akkerman <commit@dynamite.software>
2021-03-14 14:29:44 +0100

    ContainerKafkaMoveNotifierTest testing only notifier without receiver, TestContainersConfiguration extracted

--- kotlin-app/src/test/kotlin/de/igorakkerman/demo/kafka/kafka/container/ContainerKafkaMoveNotifierTest.kt
+++ kotlin-app/src/test/kotlin/de/igorakkerman/demo/kafka/kafka/container/ContainerKafkaMoveNotifierTest.kt
@@ -2,60 +2,57 @@ package de.igorakkerman.demo.kafka.kafka.container
 
 import de.igorakkerman.demo.kafka.application.Move
 import de.igorakkerman.demo.kafka.kafka.KafkaMoveNotifier
-import de.igorakkerman.demo.kafka.kafka.container.ContainerKafkaMoveNotifierTest.KafkaTestContainersConfiguration
 import de.igorakkerman.demo.kafka.springboot.Application
-import org.apache.kafka.clients.producer.ProducerConfig
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.test.utils.KafkaTestUtils
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.EnabledIf
-import org.testcontainers.containers.KafkaContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.utility.DockerImageName
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter.ofPattern
 
 @SpringBootTest(classes = [Application::class])
 @EnabledIf(expression = "\${test.container.enabled:false}", loadContext = true)
+@Import(TestContainersConfiguration::class)
 @Testcontainers
-@Import(KafkaTestContainersConfiguration::class)
 @DirtiesContext
 internal class ContainerKafkaMoveNotifierTest(
     @Autowired
     private val moveNotifier: KafkaMoveNotifier,
+
+    @Autowired
+    private val consumerFactory: ConsumerFactory<String, Move>,
+
+    @Value("\${spring.kafka.template.default-topic}")
+    private val topic: String,
+
+    @Value("\${spring.kafka.consumer.group-id}")
+    private val consumerGroupId: String,
 ) {
-    @TestConfiguration
-    @ConditionalOnProperty("test.container.enabled", havingValue = "true")
-    internal class KafkaTestContainersConfiguration {
-
-        @Container
-        val kafkaContainer = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"))
-            .apply { start() }
-
-        private fun <K, V> MutableMap<K, V>.alsoMap(vararg pairs: Pair<K, V>) = apply { putAll(pairs) }
-
-        @Bean
-        fun producerFactory(
-            kafkaProperties: KafkaProperties,
-        ) = DefaultKafkaProducerFactory<Any, Any>(
-            kafkaProperties.buildProducerProperties().alsoMap(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaContainer.bootstrapServers,
-            )
-        )
-    }
 
     @Test
     internal fun `should be reading a message`() {
-        val now = LocalDateTime.now().format(ofPattern("yyyy-MM-dd HH:mm:ss"))
-        moveNotifier.notifyPlayers(Move("Cinka", 101, now))
-        // TODO: verify message has been sent to Kafka
+        // given
+        val move = Move("Cinka", 321, LocalDateTime.now().toString())
+
+        // when
+        moveNotifier.notifyPlayers(move)
+
+        // then
+        val receivedMove =
+            // group id must be different from receiver's, so that both receive all messages
+            consumerFactory.createConsumer("$consumerGroupId-test", null)
+                .use { consumer ->
+                    consumer.subscribe(listOf(topic))
+                    return@use KafkaTestUtils.getSingleRecord(consumer, topic).value()
+                }
+
+        receivedMove shouldBe move
     }
 }
+
---
+++ kotlin-app/src/test/kotlin/de/igorakkerman/demo/kafka/kafka/container/TestContainersConfiguration.kt
@@ -0,0 +1,43 @@
+package de.igorakkerman.demo.kafka.kafka.container
+
+import de.igorakkerman.demo.kafka.application.Move
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.testcontainers.containers.KafkaContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.utility.DockerImageName
+
+@TestConfiguration
+@ConditionalOnProperty("test.container.enabled", havingValue = "true")
+internal class TestContainersConfiguration {
+
+    @Container
+    val kafkaContainer = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"))
+        .apply { start() }
+
+    private fun <K, V> MutableMap<K, V>.alsoMap(vararg pairs: Pair<K, V>) = apply { putAll(pairs) }
+
+    @Bean
+    fun consumerFactory(
+        kafkaProperties: KafkaProperties,
+    ) = DefaultKafkaConsumerFactory<String, Move>(
+        kafkaProperties.buildConsumerProperties().alsoMap(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaContainer.bootstrapServers,
+        )
+    )
+
+    @Bean
+    fun producerFactory(
+        kafkaProperties: KafkaProperties,
+    ) = DefaultKafkaProducerFactory<Any, Any>(
+        kafkaProperties.buildProducerProperties().alsoMap(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaContainer.bootstrapServers,
+        )
+    )
+}
