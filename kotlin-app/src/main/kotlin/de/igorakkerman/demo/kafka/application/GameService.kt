6b3504ade1f13c009b7b0327ad1ad6afb8fe19f1
Igor Akkerman <commit@dynamite.software>
2021-03-15 03:09:42 +0100

    GameService: logging added

--- kotlin-app/src/main/kotlin/de/igorakkerman/demo/kafka/application/GameService.kt
+++ kotlin-app/src/main/kotlin/de/igorakkerman/demo/kafka/application/GameService.kt
@@ -1,10 +1,15 @@
 package de.igorakkerman.demo.kafka.application
 
+import mu.KotlinLogging
+
 class GameService(private val moveNotifier: MoveNotifier) {
 
     val moves = ArrayList<Move>()
 
+    private val log = KotlinLogging.logger {}
+
     fun registerMove(move: Move) {
+        log.info("Registering move: $move")
         moves.add(move)
     }
 }
